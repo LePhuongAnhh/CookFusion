dabd6714c3fc7479abb044af51092a6ccec367f0
LePhuongAnhh <95765597+LePhuongAnhh@users.noreply.github.com>
2023-08-19 02:30:01 +0700

    update
    update

--- frontend/src/components/user/header/Navigation.js
+++ frontend/src/components/user/header/Navigation.js
@@ -62,15 +62,21 @@ const Navigation = () => {
                         </div>
                     </div>
                     <div className={style.nav_right}>
-                        {/* <div className={style.login}> */}
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                        </svg> &nbsp;
-                        <p >Login</p>
-                        {/* </div> */}
-                    </div>
+                        <div className={style.pulse}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+                                <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z" />
+                            </svg>
+                        </div>
 
+                        <div className={style.right_login}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                            </svg> &nbsp;
+                            
+                        </div>
+                        <p >Login</p>
+                    </div>
                 </div>
             </header >
         </>
