0fb55fda24dfd4588b0f32905a07b957e2eb3fdb
LePhuongAnhh <95765597+LePhuongAnhh@users.noreply.github.com>
2023-10-18 03:36:57 +0700

    update category

---
+++ frontend/src/components/Modal/DeleteCategory.js
@@ -0,0 +1,29 @@
+import styles from './DeleteBlog.module.scss'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
+const DeleteCategory = ({ setShowDeleteCategoryModal }) => {
+    return (
+        <div className={cx('modalDeleteIdea')}>
+            < div className={cx('modalContentDeleteIdea')}>
+                <div className={cx('header-delete')}>
+                    <h2 className={cx('containerDelete')}>Delete</h2>
+                    <div onClick={() => setShowDeleteCategoryModal(false)} className={cx('icon-exit')}>
+                        <i class="bi bi-x"></i>
+                    </div>
+                </div>
+                <hr className={cx('line')} />
+                <div className={cx('body-delete')}>
+                    < p>Are you sure you want to delete your idea? </p>
+                    {/* <p>This action cannot be undone.</p> */}
+                </div>
+                <div className={cx('footer-delete')}>
+                    < button className={cx('btn_delete')} >Delete</button>
+                    < button className={cx('btn_cancle')} onClick={() => setShowDeleteCategoryModal(false)}>Cancel</button>
+                </div>
+            </div >
+        </div >
+    )
+}
+
+export default DeleteCategory
