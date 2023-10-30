61167e17fcd93193217c88f670e70c233c6c1743
LePhuongAnhh <95765597+LePhuongAnhh@users.noreply.github.com>
2023-10-30 19:43:55 +0700

    update category api

--- frontend/src/pages/admin/Category/Category.js
+++ frontend/src/pages/admin/Category/Category.js
@@ -7,19 +7,44 @@ import Table from 'react-bootstrap/Table';
 import CreateCategory from '~/components/Modal/CreateCategory';
 import UpdateCategory from '~/components/Modal/UpdateCategory';
 import DeleteCategory from '~/components/Modal/DeleteCategory';
-
+import axios from "axios"
+import {
+    apiUrl,
+    ACCESS_TOKEN,
+    ACCOUNT_ID,
+    ROLE,
+    PROFILE_INFORMATION,
+    USERNAME
+} from "../../../constants/constants.js"
 const cx = classNames.bind(styles)
 function Category() {
     const [showCreateCategoryModal, setShowCreateCategoryModal] = useState(false);
     const [showUpdateCategoryModal, setShowUpdateCategoryModal] = useState(false);
     const [showDeleteCategoryModal, setShowDeleteCategoryModal] = useState(false);
+    const [total, setTotal] = useState(0)
+    const [list, setList] = useState([])
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await axios.get(`${apiUrl}/admin/getAllCategory`);
+                console.log(response.data)
+                if (response.data.success) {
+                    setTotal(response.data.categories.length)
+                    setList(response.data.categories)
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        )()
+    }, [])
     return (
         <>
             <div className={cx('container_fluid')}>
                 <div className={cx('row')}>
                     <div className={cx('col_12')}>
                         <div className={cx('page_title_box')}>
-                            <h4 className={cx('page_title')}>Total: 234</h4>
+                            <h4 className={cx('page_title')}>Total: {total}</h4>
                             <div onClick={() => setShowCreateCategoryModal(true)} className={cx('add-cate')}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
                                     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
@@ -40,30 +65,34 @@ function Category() {
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr className={cx("hover-actions-trigger")}>
-                                    <td>
-                                        <div className='d-flex align-items-center mt-1'>
-                                            <img
-                                                src='https://mdbootstrap.com/img/new/avatars/8.jpg'
-                                                alt=''
-                                                style={{ width: '65px', height: '65px' }}
-                                                className="rounded-2"
-                                            />
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <p className={cx('mt-2')}>Vegetarian food</p>
-                                    </td>
-                                    <td>
-                                        <p className={cx('mt-2')}>là loại thực phẩm được chế biến và thực hiện từ các nguồn thực phẩm không chứa thịt hoặc sản phẩm từ động vật</p>
-                                    </td>
-                                    <td >
-                                        <p className={cx('mt-2')}>
-                                            <i onClick={() => setShowUpdateCategoryModal(true)} className={cx("bi bi-pencil-square")} style={{ marginRight: "15px" }}></i>
-                                            {/* <i onClick={() => setShowDeleteCategoryModal(true)} className={cx("bi bi-trash")}></i> */}
-                                        </p>
-                                    </td>
-                                </tr>
+                                {
+                                    list && list.map((category, index) => (
+                                        <tr className={cx("hover-actions-trigger")}>
+                                            <td>
+                                                <div className='d-flex align-items-center mt-1'>
+                                                    <img
+                                                        src={category.image}
+                                                        style={{ width: '65px', height: '65px' }}
+                                                        className="rounded-2"
+                                                    />
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <p className={cx('mt-2')}>{category.name}</p>
+                                            </td>
+                                            <td>
+                                                <p className={cx('mt-2')}>{category.description}</p>
+                                            </td>
+                                            <td >
+                                                <p className={cx('mt-2')}>
+                                                    <i onClick={() => setShowUpdateCategoryModal(true)} className={cx("bi bi-pencil-square")} style={{ marginRight: "15px" }}></i>
+                                                    {/* <i onClick={() => setShowDeleteCategoryModal(true)} className={cx("bi bi-trash")}></i> */}
+                                                </p>
+                                            </td>
+                                        </tr>
+                                    ))
+                                }
+
                             </tbody>
                         </Table>
                     </div>
@@ -87,64 +116,33 @@ function Category() {
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr className={cx("hover-actions-trigger")}>
-                                    <td>
-                                        <div className='d-flex align-items-center mt-1'>
-                                            <p className={cx('mt-2')}>Vegetarian food</p>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <p className={cx('mt-2')}>34</p>
-                                    </td>
-                                    <td>
-                                        <div className={cx('c-flex')}>
-                                            <div className={cx('flex-mod')}>
-                                                <Link to='#'>
-                                                    <img
-                                                        src='https://mdbootstrap.com/img/new/avatars/8.jpg'
-                                                        className={cx("c-rounded-circle")}
-                                                    />
-                                                </Link>
-                                                <Link to='#'>
-                                                    <img
-                                                        src='https://mdbootstrap.com/img/new/avatars/8.jpg'
-                                                        className={cx("c-rounded-circle")}
-                                                    />
-                                                </Link>
-                                                <Link to='#'>
-                                                    <img
-                                                        src='https://mdbootstrap.com/img/new/avatars/8.jpg'
-                                                        className={cx("c-rounded-circle")}
-                                                    />
-                                                </Link>
-                                                <Link to='#'>
-                                                    <img
-                                                        src='https://mdbootstrap.com/img/new/avatars/8.jpg'
-                                                        className={cx("c-rounded-circle")}
-                                                    />
-                                                </Link>
-                                                <Link to='#'>
-                                                    <img
-                                                        src='https://mdbootstrap.com/img/new/avatars/8.jpg'
-                                                        className={cx("c-rounded-circle")}
-                                                    />
-                                                </Link>
-                                                <Link to='#'>
-                                                    <img
-                                                        src='https://mdbootstrap.com/img/new/avatars/8.jpg'
-                                                        className={cx("c-rounded-circle")}
-                                                    />
-                                                </Link>
-                                                <Link to='#'>
-                                                    <img
-                                                        src='https://mdbootstrap.com/img/new/avatars/8.jpg'
-                                                        className={cx("c-rounded-circle")}
-                                                    />
-                                                </Link>
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
+                                {
+                                    list && list.map((category, index) => (
+                                        <tr className={cx("hover-actions-trigger")}>
+                                            <td>
+                                                <div className='d-flex align-items-center mt-1'>
+                                                    <p className={cx('mt-2')}>{category.name}</p>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <p className={cx('mt-2')}>{category.recipes.length}</p>
+                                            </td>
+                                            <td>
+                                                <div className={cx('c-flex')}>
+                                                    <div className={cx('flex-mod')}>
+                                                        {category.top.length > 0 && category.top.map((user)=>(
+                                                            <Link to='#'>
+                                                            <img
+                                                                src={user.avatar} title= {user.count + " posts"}
+                                                                className={cx("c-rounded-circle")}
+                                                            />
+                                                        </Link>
+                                                        )) }
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    ))}
                             </tbody>
                         </Table>
                     </div>
