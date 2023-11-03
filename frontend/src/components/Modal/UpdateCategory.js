312d951a55a40511c2378854f189a6974b0cc6fe
LePhuongAnhh <95765597+LePhuongAnhh@users.noreply.github.com>
2023-11-04 04:44:09 +0700

    edi update cate

--- frontend/src/components/Modal/UpdateCategory.js
+++ frontend/src/components/Modal/UpdateCategory.js
@@ -1,75 +1,137 @@
-import styles from './CreateBlog.module.scss'
-import classNames from 'classnames/bind'
-import React, { useState, Component, useCallback } from 'react';
+import React, { useState } from 'react';
+import { Form, Button } from "react-bootstrap";
+import axios from 'axios';
+import { apiUrl, ACCESS_TOKEN } from '~/constants/constants';
+import classNames from 'classnames/bind';
+import styles from './CreateBlog.module.scss';
 import { useDropzone } from 'react-dropzone';
-import { Link } from 'react-router-dom'
-import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import images from '~/assets/images'
-import { ACCESS_TOKEN } from '~/constants/constants';
+const cx = classNames.bind(styles);
 
-const cx = classNames.bind(styles)
-function UpdateCategory({ setShowUpdateCategoryModal }) {
+function UpdateCategory({ setShowUpdateCategoryModal, showUpdateCategoryModal, categoryData }) {
     const accessToken = localStorage.getItem(ACCESS_TOKEN);
-    const onDrop = useCallback((acceptedFiles) => {
-        // Xử lý tệp đã được chọn ở đây, ví dụ: tải lên máy chủ hoặc hiển thị trước tệp.
 
-        // Ví dụ hiển thị tên tệp đã chọn:
-        acceptedFiles.forEach((file) => {
-            console.log('File selected:', file.name);
-        });
-    }, []);
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [imageChosen, setImageChosen] = useState(false);
+
+    const { name, image, description, _id } = categoryData;
+    const [updatedName, setUpdatedName] = useState(name);
+    const [updatedDescription, setUpdatedDescription] = useState(description);
+    const [updatedId, setUpdatedId] = useState(_id);
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const formData = new FormData();
+            formData.append('_id', updatedId);
+            formData.append('name', updatedName);
+            formData.append('image', selectedFile || image);
+            formData.append('description', updatedDescription);
+
+            const response = await axios.patch(`${apiUrl}/category/update`, formData, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            });
+            setShowUpdateCategoryModal(false);
+        } catch (error) {
+            console.error('Error updating category:', error);
+        }
+    };
+
+    const onDrop = (acceptedFiles) => {
+        const file = acceptedFiles[0];
+        setSelectedFile(file);
+        setImageChosen(true);
+    };
+
 
     const { getRootProps, getInputProps } = useDropzone({
         onDrop,
-        accept: 'image/*', // Chấp nhận tệp ảnh
+        accept: 'image/*',
     });
+
     return (
-        <div className={cx('modalDeleteIdea')}>
-            <div className={cx('modalCreateCategory')}>
+        <div className={cx('modalUpdateCategory')} style={{ display: showUpdateCategoryModal ? 'block' : 'none' }}>
+            <div className={cx('modalUpdateCategory')}>
                 <div>
-                    <div className={cx('createIdeaHeader')}>
-                        <h1 className={cx('createIdea')}>Update Category</h1>
+                    <div className={cx('updateCategoryHeader')}>
+                        <h1 className={cx('updateCategory')}>Update Category</h1>
                         <div className={cx('exit_cmt_modal')} onClick={() => setShowUpdateCategoryModal(false)}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
                             </svg>
                         </div>
                     </div>
-                    <div className={cx('body-cate')}>
-                        <Form>
+                    <div className={cx('body-cate')} style={{ height: '341px', overflow: "auto" }}>
+                        <Form onSubmit={handleSubmit}>
                             <Form.Group className="mb-4 display-3" controlId="formGroupName">
-                                <Form.Control className={cx('form-text')} type="text" placeholder="Name" />
-                            </Form.Group>
-                            <Form.Group className="mb-4" controlId="formGroupGender">
-                                <Form.Select className={cx('form-text')}>
-                                    <option className={cx('form-text')} value=''>Select type category</option>
-                                    <option className={cx('form-text')} value="male">Article</option>
-                                    <option className={cx('form-text')} value="female">Recipe</option>
-                                </Form.Select>
+                                <Form.Control
+                                    className={cx('form-text')}
+                                    type="text"
+                                    name="name"
+                                    placeholder="Name"
+                                    value={updatedName}
+                                    onChange={(e) => setUpdatedName(e.target.value)}
+                                    required
+                                />
                             </Form.Group>
                             <Form.Group className={cx("mb-4", "form-text")} controlId="description">
-                                <Form.Control className={cx('form-text')} as="textarea" rows={4} placeholder="Description" />
+                                <Form.Control
+                                    className={cx('form-text')}
+                                    as="textarea"
+                                    name="description"
+                                    rows={7}
+                                    placeholder="Description"
+                                    value={updatedDescription}
+                                    onChange={(e) => setUpdatedDescription(e.target.value)}
+                                    required
+                                />
                             </Form.Group>
-                            <div className={cx('upload-file')}>
-                                <div className={cx('dropzone')} id="dropzone" data-dropzone="data-dropzone" {...getRootProps()}>
-                                    <input {...getInputProps()} />
-                                    <div className={cx('dz-message')} data-dz-message="data-dz-message">
-                                        <div className={cx('dz-message-text')}>
-                                            <i className="bi bi-upload"></i> &nbsp;
-                                            Choose file
+                            <Form.Group controlId="formGroupImage">
+                                <div className={cx('image-select-container')}>
+                                    {selectedFile ? ( // Display the selected file if available
+                                        <img
+                                            src={URL.createObjectURL(selectedFile)}
+                                            alt="Selected file"
+                                            className={cx('image-select')}
+                                        />
+                                    ) : ( // Otherwise, display the existing image
+                                        <img
+                                            src={image}
+                                            alt="Category"
+                                            className={cx('image-select')}
+                                        />
+                                    )}
+                                </div>
+                                <div className={cx('upload-file')}>
+                                    {/* Dropzone to upload new image */}
+                                    <div className={cx('dropzone')} id="dropzone" data-dropzone="data-dropzone" {...getRootProps()}>
+                                        <input {...getInputProps()} />
+                                        <div className="dz-preview dz-preview-multiple m-0 d-flex flex-column">
+                                            {/* Render nothing here as we're displaying the selected image above */}
+                                        </div>
+                                        <div
+                                            className={cx('dz-message')}
+                                            data-dz-message="data-dz-message"
+                                        >
+                                            <div className={cx('dz-message-text')}>
+                                                <i className="bi bi-upload"></i> &nbsp;
+                                                {imageChosen ? 'Update image' : 'Choose image'}
+                                            </div>
                                         </div>
                                     </div>
-                                    <div className="dz-preview dz-preview-multiple m-0 d-flex flex-column"></div>
                                 </div>
-                            </div>
-                            <Button className={cx('btn-submit')} variant="primary" type="submit">
-                                Submit
-                            </Button>
+                            </Form.Group>
+
                         </Form>
                     </div>
+                    <Button className={cx('btn-submit')} variant="primary" type="submit" onClick={handleSubmit}>
+                        Submit
+                    </Button>
                 </div>
-            </div >
-        </div >
+            </div>
+        </div>
     );
 }
 
