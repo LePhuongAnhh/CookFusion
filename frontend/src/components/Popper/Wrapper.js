69a3fb57117fbf64a841a102f78551c13d3438b9
ManhTung64 <94000462+ManhTung64@users.noreply.github.com>
2023-12-12 20:30:52 +0700

    Update Wrapper.js

--- frontend/src/components/Popper/Wrapper.js
+++ frontend/src/components/Popper/Wrapper.js
@@ -1,13 +1,13 @@
 // import { children } from "react";
-// import styles from "./popper.module.scss"
-// import classNames from "classnames/bind"
+import styles from "./popper.module.scss"
+import classNames from "classnames/bind"
 
-// const cx = classNames.bind(styles);
-// function Wrapper({ children }) {
-//     return (
-//         <div className={cx('wrapper')}>{children}
+const cx = classNames.bind(styles);
+function Wrapper({ children }) {
+    return (
+        <div className={cx('wrapper')}>{children}
 
-//         </div>
-//     );
-// }
+        </div>
+    );
+}
 export default Wrapper;
